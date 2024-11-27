1c1f21cc2e4ced01564cce37e635c6c9d949514e
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-27 12:19:46 +0200

    initial commit

---
+++ src/main/java/com/tadiwa/otp_example/OtpExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tadiwa.otp_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtpExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tadiwa/otp_example/OtpExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadiwa.otp_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
